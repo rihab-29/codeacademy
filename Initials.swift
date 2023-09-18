e718317e0414290a06170b1afc15cf1e33eecfc8
rihab-29 <rihab.jrandi02@gmail.com>
2023-09-18 18:57:54 +0100

    1st projet

---
+++ Initials.swift
@@ -0,0 +1,8 @@
+// I'm an adult who still admires disney
+print(" R R R R           J J J J J")
+print(" R       R             J    ")
+print(" R       R             J    ")
+print(" R R R                 J    ")
+print(" R   R             J   J    ")
+print(" R     R           J   J    ")
+print(" R       R           J J    ")
